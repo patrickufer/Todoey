a42371a12d22a7c1188372c4abec1b9c61eb0a64
patrickufer <patrickufer18@gmail.com>
2019-06-19 14:51:10 -0400

    Fixed bugs with checkmark

---
+++ Todoey/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Patrick Ufer on 6/18/19.
+//  Copyright © 2019 Patrick Ufer. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
